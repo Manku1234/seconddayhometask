262bc143f86de286f4da3c4e4b70b434f9bfc041
Manku1234 <manjari040398@gmail.com>
2020-04-14 10:53:25 +0530

    first commit

---
+++ api_auth/index.js
@@ -0,0 +1,26 @@
+const config = require("config");
+const mongoose = require("mongoose");
+const usersRoute = require("./routes/user.route");
+const express = require("express");
+const app = express();
+
+
+
+if (!config.get("myprivatekey")) {
+  console.error("FATAL ERROR: myprivatekey is not defined.");
+  process.exit(1);
+}
+
+
+mongoose
+  .connect("mongodb://localhost/api_authorization", { useNewUrlParser: true })
+  .then(() => console.log("Connected to MongoDB..."))
+  .catch(err => console.error("Could not connect to MongoDB..."));
+
+
+app.use(express.json());
+
+app.use("/api/users", usersRoute);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}...`));
